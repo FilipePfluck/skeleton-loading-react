e6af05ed95bd917882b77f3820dd97fe08cdbda7
Filipe Pfluck <filipepfluckdev@gmail.com>
2022-03-25 16:35:10 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-skeleton-loading'
+import 'react-skeleton-loading/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
