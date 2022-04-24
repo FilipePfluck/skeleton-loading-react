988cbc6ac1a4389edbbd4ea62b102981ee013d43
Filipe Pfluck <filipepfluckdev@gmail.com>
2022-04-24 19:50:27 -0300

    feat: first working version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-import { ExampleComponent } from 'react-skeleton-loading'
+import { Skeleton } from 'react-skeleton-loading'
 import 'react-skeleton-loading/dist/index.css'
 
+interface TestProps {
+  user: {
+    avatar: string
+    name: string
+    description: string
+  }
+}
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [isLoading, setIsLoading] = useState(true)
+
+  const Test: React.FC<TestProps> = ({user, children}) => {
+    return ( 
+        <div style={{padding: 12, width: 400}}>
+            <header style={{display: 'flex', alignItems: 'center', marginBottom: 8}}>    
+                <img 
+                    src={user.avatar} 
+                    alt={user.name} 
+                    style={{width: 32, height: 32, borderRadius: '50%', marginRight: 8}}
+                />
+                <p style={{margin: 0}}>{user.name}</p>
+            </header>
+            <p>{user.description}</p>
+            {children}
+        </div>
+    )
+    }
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false)
+    },2000)
+  },[])
+
+  const fakeUser = {
+    name: 'Jonathan Doestar',
+    avatar: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit'
+  }
+
+  const user = {
+    name: 'Filipe Pfluck',
+    avatar: 'https://avatars.githubusercontent.com/u/62773200?v=4',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit'
+  }
+
+  return <Skeleton 
+      isLoading={isLoading} 
+      exampleProps={{user: fakeUser}}
+    >
+    <Test user={user}>
+      <p>teste</p>
+    </Test>
+  </Skeleton>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,112 @@
-import * as React from 'react'
+import React, { useRef, useEffect, useState, Fragment, /*FC,*/ CSSProperties, ReactElement } from "react"
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+export interface SkeletonProps/*<T>*/ {
+  children: React.ReactNode
+  isLoading: boolean;
+  exampleProps: any; 
+  defaultStyles?: CSSProperties;
+  className?: string;
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Skeleton = /*<T extends unknown>*/({ children, isLoading, exampleProps={}, defaultStyles = {}, className }: SkeletonProps/*<T>*/): ReactElement | null=> {
+  const fakeComponentRef = useRef<HTMLDivElement>(null)
+
+  const [show, setShow] = useState(false)
+
+  useEffect(()=>{
+      setTimeout(()=>{
+          setShow(true)
+      },1)
+  })
+  
+  const texts = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong']
+  const contents = ['img', 'video', 'button', 'input', 'textarea', 'select']
+
+  const styleProps = ['borderRadius', 'padding', 'margin', 'marginRight', 'marginLeft', 'marginTop', 'marginBottom', 'paddingTop', 'paddingBottom', 'paddingLeft', 'paddingRight', 'display', 'alignItems', 'justifyContent', 'flexDirection', 'gridTemplateColumns', 'gridTemplateRows', 'gridGap', 'columnGap', 'rowGap', 'gap', 'gridTemplateAreas', 'marginBlockStart', 'marginBlockEnd' ]
+
+  const renderElement = (element: HTMLElement) => {
+      const object = {}
+      styleProps.forEach(s => Object.assign(object, {[s]: element.style[s]}))
+
+      if(element.localName === 'header'){
+        console.log(object)
+      }
+
+      if(texts.includes(element.localName)){
+          const fontSize = +document.defaultView!.getComputedStyle(element, null)["fontSize"].replace('px','')
+          const lineHeight = +document.defaultView!.getComputedStyle(element, null)["lineHeight"].replace('px','') | fontSize * 1.2
+          const numberOfLines = Math.round(element.offsetHeight / lineHeight)
+          const lineMarginBottom = lineHeight - fontSize
+
+          const lines = []
+
+          for(let i=0; i<numberOfLines; i++){
+              lines.push(i)
+          }
+
+          return(
+              <div style={{display: 'flex', flexDirection: 'column'}}>
+                  {lines.map(line => (
+                      <div 
+                      style={{
+                          width: element.offsetWidth,
+                          ...object,
+                          ...defaultStyles,
+                          height: fontSize,
+                          marginBottom: lineMarginBottom
+                      }}
+                      className={`${styles.shimmer} ${className}`}
+                      key={"line"+line}
+                  />))
+                  }
+              </div>  
+          )
+      }
+
+      if(contents.includes(element.localName)){
+          return (
+              <div 
+                  style={{
+                      width: element.offsetWidth,
+                      height: element.offsetHeight,
+                      ...object,
+                      ...defaultStyles
+                  }}
+                  className={`${styles.shimmer} ${className}`}
+              />
+          )
+      }
+
+      return (
+          <div 
+              style={{
+                  width: element.offsetWidth,
+                  height: element.offsetHeight,
+                  ...object,          
+              }}
+          >
+              {!!element.children 
+                  ? Array.from(element.children)
+                      .map(child => renderElement(child as HTMLElement))
+                  : null
+              } 
+          </div>
+      )
+  }
+
+  return <Fragment>
+    <div style={isLoading ? {visibility: 'hidden', position: 'absolute'} : {}} ref={fakeComponentRef}>
+        
+        {   // @ts-ignore
+            typeof children === 'object' && children?.props ? {
+            ...children, 
+            props: isLoading 
+                ? exampleProps 
+                // @ts-ignore
+                : children?.props || {}
+        } : children}
+    </div>
+    {show && isLoading && renderElement(fakeComponentRef.current as HTMLDivElement)}
+  </Fragment>
 }
